9b219c89a0fc6c92cfe750e67758df3033276c89
Daniel Hongel and Josh Mabry <daniel@>
2019-06-18 14:34:58 -0700

    add logic to output current balance to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 //back-end logic
+
+//accountArr logic
+
 function AccountArr() {
   this.accounts = [],
   this.currentId = 0;
@@ -14,6 +17,20 @@ AccountArr.prototype.assignId = function () {
   return this.currentId;
 };
 
+AccountArr.prototype.findAccount = function (id) {
+  for (i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i]) {
+      if (this.accounts[i].id == id) {
+        return this.accounts[i];
+      }
+    }
+  };
+
+  return false;
+};
+
+//account logic
+
 function Account(firstName, lastName, balance) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -23,3 +40,50 @@ function Account(firstName, lastName, balance) {
 Account.prototype.fullName = function () {
   return this.firstName + ' ' + this.lastName;
 };
+
+Account.prototype.findBalance = function () {
+  return this.balance
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//front-end logic
+
+var accountArr1 = new AccountArr();
+
+var account1 = new Account('Josh', 'Mabry', 0);
+var account2 = new Account('Daniel', 'Hongel', 50);
+accountArr1.addAccount(account1);
+accountArr1.addAccount(account2);
+
+
+
+function showBalance() {
+  $('#balance').text(this.balance);
+}
+
+$(document).ready(function() {
+  $('form.form-one').submit(function(event) {
+    event.preventDefault();
+    var inputtedFirstName = $('input#first-name').val();
+    var inputtedLastName = $('input#last-name').val();
+    var inputtedInitialDeposit = $('input#initial-deposit').val();
+    $('input#first-name').val('');
+    $('input#last-name').val('');
+    $('input#initial-deposit').val('');
+    console.log(inputtedFirstName);
+    var newAccount = new Account(inputtedFirstName, inputtedLastName, inputtedInitialDeposit);
+    accountArr1.addAccount(newAccount);
+    $('#balance').text('$' + inputtedInitialDeposit);
+  })
+});
